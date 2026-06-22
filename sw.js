05e5bedbe341c474bd7028df04c06ae6e1380fad
Vadim <149485396+florichdev@users.noreply.github.com>
2026-06-22 11:05:18 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+var CACHE = "florichdev-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./assets/style.css",
+  "./assets/app.js",
+  "./assets/images/logo-icon.svg",
+  "./assets/images/og-image.png",
+  "./favicon.svg",
+  "./manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (key) { return key !== CACHE; }).map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request).then(function (response) {
+        if (!response || response.status !== 200 || response.type === "opaque") {
+          return response;
+        }
+        var copy = response.clone();
+        caches.open(CACHE).then(function (cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      });
+    })
+  );
+});
